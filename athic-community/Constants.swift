bfc546de9b3b0ccbc34eae0b66973e1d37c7af62
MarkBooster <m.booster@outlook.com>
2016-09-06 12:12:12 +0200

    Sign in UI custom classes complete

---
+++ athic-community/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  athic-community
+//
+//  Created by Mark Booster on 06-09-16.
+//  Copyright © 2016 Mark Booster. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
