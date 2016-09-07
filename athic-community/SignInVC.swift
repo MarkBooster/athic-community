4155d5c25b674ff904c91291bba4d92e09ffcda5
MarkBooster <m.booster@outlook.com>
2016-09-07 19:52:09 +0200

    Finished the facebook auth

--- athic-community/SignInVC.swift
+++ athic-community/SignInVC.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    @IBAction func facebookBtnPressed(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("MARK: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("MARK: User cancelled Facebook authentication")
+            } else {
+                print("MARK: Succesfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("MARK: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("MARK: Succesfully authenticated with Firebase")
+            }
+        })
+        
     }
 
 }
