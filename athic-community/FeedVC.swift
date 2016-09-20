6967a8a33d66cc109b4a23d08b42c51f6a4035f5
MarkBooster <m.booster@outlook.com>
2016-09-20 20:16:36 +0200

    Parsed the Firebase data in the UI

--- athic-community/FeedVC.swift
+++ athic-community/FeedVC.swift
@@ -50,10 +50,16 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell 
+        
+        let post = posts[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
     }
-    
-    
 
     @IBAction func signOutPressed(_ sender: AnyObject) {
         let keychainResult = KeychainWrapper.removeObjectForKey(KEY_UID)
