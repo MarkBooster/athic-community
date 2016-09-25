e336c25cf8a12a627eec850a7e5d209746df6f1f
MarkBooster <m.booster@outlook.com>
2016-09-25 17:08:38 +0200

    Completed the firebase post

--- athic-community/FeedVC.swift
+++ athic-community/FeedVC.swift
@@ -115,10 +115,31 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
                 } else {
                     print("MARK: Succesfully Uploaded image to firebase")
                     let downloadURL = metadata?.downloadURL()?.absoluteString
+                    if let url = downloadURL {
+                        self.postToFirebase(imgUrl: url)
+                    }
+                    
                 }
             }
         }
     
+    }
+    
+    func postToFirebase(imgUrl: String) {
+        let post: Dictionary<String, Any> = [
+            "caption": captionField.text!,
+            "imageUrl": imgUrl,
+            "likes": 0
+        ]
+        
+        let firebasePost = DataService.ds.REF_POSTS.childByAutoId()
+        firebasePost.setValue(post)
+        
+        captionField.text = ""
+        imageSelected = false
+        imageAdd.image = UIImage(named: "add-image")
+        
+        tableView.reloadData()
     }
     
     @IBAction func signOutPressed(_ sender: AnyObject) {
